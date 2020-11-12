61451e8174a03a22fb7333de67d1449e9d2fc20b
Alex Montalvo <almosa01@floridauniversitaria.es>
2020-11-12 21:24:11 +0100

    Actividad finalizada-

--- PlaceMyBetDI/App.js
+++ PlaceMyBetDI/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, ActivityIndicator,} from 'react-native';
 import {Header, Image, Avatar, Input, Button, Icon, Card } from 'react-native-elements';
 import { LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, } from 'react-native/Libraries/NewAppScreen';
 
@@ -15,36 +15,37 @@ const App = () => {
   return (
     <View style={styles.mainColor}>
         <View style={{backgroundColor:'#02A7F0', height:96, color: 'white', fontWeight: 'bold', fontSize: 25, borderRadius:10,
-          marginBottom:20, marginTop:20, marginLeft:30, marginRight:30 }}/>
+          marginBottom:20, marginTop:20, marginLeft:30, marginRight:30, justifyContent:'center', alignItems:'center'}}>
+            <Image source={{ uri: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2b84.png' }} style={{ width: 80, height: 80}} />
+        </View>
         <View style={styles.body}>
           <View style={styles.ViewMain}>
             <View style={styles.ViewImage}>
-
+              <Image source={{ uri: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2b84.png' }} style={{ width: 50, height: 50 }} />
             </View>
             <View style={styles.ViewTexto}>
-            <Text>DDDD</Text>
+              <Text>Descripción...</Text>
             </View>
           </View>
           <View style={styles.ViewMain}>
             <View style={styles.ViewImage}>
-
+            <Image source={{ uri: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2b84.png' }} style={{ width: 50, height: 50 }} />
             </View>
             <View style={styles.ViewTexto}>
-            <Text>DDDD</Text>
+              <Text>Descripción...</Text>
             </View>
           </View>
           <View style={styles.ViewMain}>
             <View style={styles.ViewImage}>
-
+            <Image source={{ uri: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2b84.png' }} style={{ width: 50, height: 50 }} />
             </View>
             <View style={styles.ViewTexto}>
-            <Text>DDDD</Text>
+              <Text>Descripción...</Text>
             </View>
           </View>
-
         </View>
-        <View style={styles.footer}>
-
+        <View style={{backgroundColor:'#4990B0',height:80,borderRadius:10,marginLeft:10,marginBottom:10,marginRight:10, justifyContent:'center', alignItems:'flex-end'}}>
+          <Button title={'Back to Main'} titleStyle={{color:'white', fontWeight:'bold'}} buttonStyle={{width:140, height:40, backgroundColor:'#2A9ACC', marginRight:20}}/>
         </View>
     </View>
   );
@@ -71,16 +72,8 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize:13,
   },
-  footer: {
-    backgroundColor:'#4990B0',
-    height:80,
-    borderRadius:10,
-    marginLeft:10,
-    marginBottom:10,
-    marginRight:10
-  },
   ViewTexto: {
-    textAlign:"right",
+    justifyContent:"center",
     height:102,
     width:217,
     marginTop:19,
@@ -96,6 +89,8 @@ const styles = StyleSheet.create({
     width:85,
     height:69,
     borderWidth:2,
+    paddingLeft:15,
+    justifyContent:"center",
     borderColor:'black',
     marginTop:32,
     marginLeft: 11,
